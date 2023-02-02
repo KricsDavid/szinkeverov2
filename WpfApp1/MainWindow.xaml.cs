e2b48d46452d95502381f87bce02025b99e9a8d0
KricsDavid <71211940+KricsDavid@users.noreply.github.com>
2023-02-02 10:14:21 +0100

    Add files via upload

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -43,6 +43,20 @@ namespace WpfApp1
             //this.Background=
             rctTeglalap.Fill = new SolidColorBrush(Color.FromRgb(Convert.ToByte(sliPiros.Value), Convert.ToByte(sliZold.Value), Convert.ToByte(sliKek.Value)));
         }
+
+        private void btnRogzit_Click(object sender)
+        {
+
+        }
+        private void btnTorol_Click(object sender)
+        {
+
+        }
+
+        private void btnUrit_Click(object sender)
+        {
+
+        }
     }
 
 }
